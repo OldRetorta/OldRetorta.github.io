d050c2923ab11805d8c31d10f225a27afe01f2c0
OldRetorta <30272539+OldRetorta@users.noreply.github.com>
2017-07-30 10:30:13 +0300

    Dot & rectangle

--- SimpleNumbers/src/SimpleNumbers/SimpleNumbers.java
+++ SimpleNumbers/src/SimpleNumbers/SimpleNumbers.java
@@ -6,6 +6,7 @@ public class SimpleNumbers {
 
     public static void main(String[] args) {
 
+        // Slack comment
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Enter the number");
